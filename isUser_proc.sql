715fb4d11d6f9243ca1f84d8aabaa17bce34d5f6
Bharadwaj Ramachandran <zbram@bharadwajs-mbp.attlocal.net>
2020-09-12 01:31:57 -0700

    isUser

--- isUser_proc.sql
+++ isUser_proc.sql
@@ -2,13 +2,35 @@ CREATE DEFINER=`skippeddbadmin`@`%` PROCEDURE `isUser`(user varchar(50),passRese
 BEGIN
 
     DECLARE verifiedUser INT DEFAULT 0;
+    DECLARE user INT DEFAULT 0;
     
-    Update recruiter
-    Set passwordResetCode = passResetCode
-    Where orgEmail = user;
+    Select recruiterID into user
+    From recruiter
+    Where orgEmail =user;
     
-	SELECT EXISTS(select *
-	FROM recruiter r WHERE  r.passwordResetCode = passResetCode AND r.verificationCode = 'VERIFIED' AND TRIM(LOWER(r.orgEmail)) = TRIM(LOWER(user))) newOrg INTO verifiedUser;
+    IF(user != 0) THEN
+    
+		Update recruiter
+		Set passwordResetCode = passResetCode
+		Where orgEmail = user;
+		
+		SELECT EXISTS(select *
+		FROM recruiter r WHERE  r.passwordResetCode = passResetCode AND r.verificationCode = 'VERIFIED' AND TRIM(LOWER(r.orgEmail)) = TRIM(LOWER(user))) newOrg INTO verifiedUser;
+	ELSE
+		Select candidateID into user
+		From candidate
+		Where email =user;
+		
+        IF(user != 0) THEN
+			Update candidate
+			Set passwordResetCode = passResetCode
+			Where email = user;
+			
+			SELECT EXISTS(select *
+			FROM candidate r WHERE  r.passwordResetCode = passResetCode AND r.verificationCode = 'VERIFIED' AND TRIM(LOWER(r.email)) = TRIM(LOWER(user))) newOrg INTO verifiedUser;
+		END IF;
+    END IF;
     #passwordResetCode
+    
     Select verifiedUser;
 END
