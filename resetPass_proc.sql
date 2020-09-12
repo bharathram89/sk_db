fdb0c141d40b81c9cfe0b3a1db22098c8e405e54
Bharadwaj Ramachandran <zbram@bharadwajs-mbp.attlocal.net>
2020-09-12 02:15:27 -0700

    candidate reset flow

--- resetPass_proc.sql
+++ resetPass_proc.sql
@@ -1,12 +1,25 @@
 CREATE DEFINER=`skippeddbadmin`@`%` PROCEDURE `resetPass`(userName varchar(50), code varchar(10),pass varchar(50))
 BEGIN
 	DECLARE User INT DEFAULT 0;
+    
     Select recruiterID INTO User
     From recruiter
     Where passwordResetCode = code AND orgEmail = userName;
     
     IF(User=0) THEN
-		Select 'USER NOT FOUND' as result;
+    
+		Select candidateID INTO User
+		From candidate
+		Where passwordResetCode = code AND email = userName;
+        IF(User!=0) THEN
+			Update candidate
+			Set password = pass , passwordResetCode='Complete'
+			Where candidateID = User;
+            
+			Select User as result;
+        ELSE
+			Select 'USER NOT FOUND' as result;
+        END IF;
     ELSE
 		Update recruiter
         Set password = pass , passwordResetCode='Complete'
